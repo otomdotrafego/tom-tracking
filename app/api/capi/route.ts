fa832363ad296b6e3a9d294d843ba590608e70c0
Elton <premfigueiredo@gmail.com>
2026-09-16 09:15:23 -0300

    feat: test event code CAPI

--- app/api/capi/route.ts
+++ app/api/capi/route.ts
@@ -26,6 +26,7 @@ async function enviarEventoMeta(
         event_name: eventName,
         event_time: Math.floor(Date.now() / 1000),
         action_source: "other",
+        test_event_code: "TEST30215",
         user_data: {
           ph: userData.phone ? [userData.phone] : undefined,
           em: userData.email ? [userData.email] : undefined,
